b8b6d78bfaaac30a223513e0d1daa4a0064b28e9
dusanrsc <dusanrosic25.06.1997@gmail.com>
2025-07-06 23:33:08 +0200

    v0.01.0-Alpha

---
+++ src/data.py
@@ -0,0 +1,7 @@
+# Metadata.
+__version__: str = "v0.01.0-Alpha"
+__version_date__: str = "06.july.2025"
+__developer_tag__ : str = "@dusanrsc"
+
+# Project Data.
+PROJECT_NAME = "NotOS"
--- src/main.py
+++ src/main.py
@@ -1,26 +1,59 @@
-import flet as ft
+# Imports Section.
+# Importing Sub-Modules Section.
+from flet import *
+import data
 
+# Program Settings Section.
+# CONSTANTS Section.
+WINDOW_WIDTH  = 400
+WINDOW_HEIGHT = 850
 
-def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+METADATA = f"{data.PROJECT_NAME} {data.__version__} by: {data.__developer_tag__}"
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
+# Hexadecimal Color CONSTANTS Section.
+# Basic Hexadecimal Color CONSTANTS Section.
+RED   = "#FF0000"
+GREEN = "#00FF00"
+BLUE  = "#0000FF"
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
-        )
-    )
+BLACK = "#000000"
+GRAY  = "#333333"
+WHITE = "#FFFFFF"
 
+ALPHA = GREEN
 
-ft.app(main)
+# Custom Hexadecimal Color CONSTANTS Section.
+DARK_GREEN = "#005500"
+
+# Default Hexadecimal CONSTANTS Colors Section.
+DEFAULT_DARK_MODE  = BLACK
+DEFAULT_LIGHT_MODE = WHITE
+
+DEFAULT_PAGE_COLOR = GRAY
+DEFAULT_SCREEN_AB_COLOR = DEFAULT_DARK_MODE
+
+# Main Program Logic Section.
+def main(page: Page):
+
+    # Main Page Window Settings Section
+    page.title = METADATA
+    page.window.width = WINDOW_WIDTH
+    page.window.height = WINDOW_HEIGHT
+    page.window.resizable = False       # Conditionaly
+    page.bgcolor = DEFAULT_PAGE_COLOR
+    page.update()
+
+    # Creating Container Widget.
+    screen_abstraction = Container(
+        border_radius=35,
+        expand=True,
+        bgcolor=DEFAULT_SCREEN_AB_COLOR,
+        alignment=alignment.bottom_center,
+        
+        content=Container(height=50, expand=True, bgcolor=DARK_GREEN))
+    
+    # Rendering Parent Container Widget Into A Page. 
+    page.add(screen_abstraction)
+    
+# Rendering The Main Window - Starting The Program!
+app(target=main)
